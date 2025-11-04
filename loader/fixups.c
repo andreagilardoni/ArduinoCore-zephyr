11d67f50fd4f504acbcf2172d86bdf58762a0126
Andrea Gilardoni <a.gilardoni@arduino.cc>
2025-11-04 17:05:01 +0100

    [TMP] making changes to loader to make espressif platform compile

--- loader/fixups.c
+++ loader/fixups.c
@@ -1,3 +1,4 @@
+#ifndef ESP_PLATFORM
 #include <cmsis_core.h>
 #include <zephyr/init.h>
 
@@ -246,3 +247,5 @@ int disable_vrefbuf() {
 
 SYS_INIT(disable_vrefbuf, POST_KERNEL, 0);
 #endif
+
+#endif // ESP_PLATFORM
