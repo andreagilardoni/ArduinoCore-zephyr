ad798acb7fd79a7f1f52e36135ab17b5411c00de
Andrea Gilardoni <a.gilardoni@arduino.cc>
2026-01-07 13:06:47 +0100

    SocketWrapper: split definition and implementation

---
+++ libraries/SocketWrapper/SocketWrapper.cpp
@@ -0,0 +1,289 @@
+#include "SocketWrapper.h"
+
+static void socket_deleter(int *fd) {
+    if (fd && *fd != -1) {
+        ::close(*fd);
+    }
+    delete fd;
+}
+
+ZephyrSocketWrapper::ZephyrSocketWrapper(int fd)
+: sock_fd(std::shared_ptr<int>(fd < 0 ? nullptr : new int(fd), socket_deleter)) {
+}
+
+bool ZephyrSocketWrapper::connect(const char *host, uint16_t port) {
+    // Resolve address
+    struct addrinfo hints = {0};
+    struct addrinfo *res = nullptr;
+    bool rv = true;
+    int raw_sock_fd;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int resolve_attempts = 100;
+    int ret;
+
+    while (resolve_attempts--) {
+        ret = getaddrinfo(host, String(port).c_str(), &hints, &res);
+
+        if (ret == 0) {
+            break;
+        } else {
+            k_sleep(K_MSEC(1));
+        }
+    }
+
+    if (ret != 0) {
+        rv = false;
+        goto exit;
+    }
+
+    raw_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sock_fd =
+        std::shared_ptr<int>(raw_sock_fd < 0 ? nullptr : new int(raw_sock_fd), socket_deleter);
+    if (!sock_fd) {
+        rv = false;
+
+        goto exit;
+    }
+
+    if (::connect(*sock_fd, res->ai_addr, res->ai_addrlen) < 0) {
+        sock_fd = nullptr;
+        rv = false;
+        goto exit;
+    }
+
+exit:
+    if (res != nullptr) {
+        freeaddrinfo(res);
+        res = nullptr;
+    }
+
+    return rv;
+}
+
+bool ZephyrSocketWrapper::connect(IPAddress host, uint16_t port) {
+
+    const char *_host = host.toString().c_str();
+    int raw_sock_fd;
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, _host, &addr.sin_addr);
+
+    raw_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sock_fd =
+        std::shared_ptr<int>(raw_sock_fd < 0 ? nullptr : new int(raw_sock_fd), socket_deleter);
+    if (!sock_fd) {
+        return false;
+    }
+
+    if (::connect(*sock_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        sock_fd = nullptr;
+        return false;
+    }
+
+    return true;
+}
+
+#if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
+bool ZephyrSocketWrapper::connectSSL(const char *host, uint16_t port, const char *ca_certificate_pem) {
+
+    // Resolve address
+    struct addrinfo hints = {0};
+    struct addrinfo *res = nullptr;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int resolve_attempts = 100;
+    int ret;
+    bool rv = false;
+    int raw_sock_fd;
+
+    sec_tag_t sec_tag_opt[] = {
+        CA_CERTIFICATE_TAG,
+    };
+
+    struct timeval timeout_opt = {
+        .tv_sec = 0,
+        .tv_usec = 100000,
+    };
+
+    while (resolve_attempts--) {
+        ret = getaddrinfo(host, String(port).c_str(), &hints, &res);
+
+        if (ret == 0) {
+            break;
+        } else {
+            k_sleep(K_MSEC(1));
+        }
+    }
+
+    if (ret != 0) {
+        goto exit;
+    }
+
+    if (ca_certificate_pem != nullptr) {
+        ret = tls_credential_add(CA_CERTIFICATE_TAG, TLS_CREDENTIAL_CA_CERTIFICATE,
+                                    ca_certificate_pem, strlen(ca_certificate_pem) + 1);
+        if (ret != 0) {
+            goto exit;
+        }
+    }
+
+    raw_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TLS_1_2);
+    sock_fd =
+        std::shared_ptr<int>(raw_sock_fd < 0 ? nullptr : new int(raw_sock_fd), socket_deleter);
+    if (!sock_fd) {
+        goto exit;
+    }
+
+    if (setsockopt(*sock_fd, SOL_TLS, TLS_HOSTNAME, host, strlen(host)) ||
+        setsockopt(*sock_fd, SOL_TLS, TLS_SEC_TAG_LIST, sec_tag_opt, sizeof(sec_tag_opt)) ||
+        setsockopt(*sock_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout_opt, sizeof(timeout_opt))) {
+        goto exit;
+    }
+
+    if (::connect(*sock_fd, res->ai_addr, res->ai_addrlen) < 0) {
+        goto exit;
+    }
+
+    rv = true;
+    is_ssl = true;
+
+exit:
+    if (res != nullptr) {
+        freeaddrinfo(res);
+        res = nullptr;
+    }
+
+    if (!rv && *sock_fd >= 0) {
+        sock_fd = nullptr;
+    }
+    return rv;
+}
+#endif
+
+int ZephyrSocketWrapper::available() {
+    int count = 0;
+    if (is_ssl) {
+        /*
+            TODO: HACK:
+            The correct colution would be to call
+            ::recv(sock_fd, &ssl_sock_temp_char, 1, MSG_PEEK | MSG_DONTWAIT);
+            but it doesn't seem to work. Instead, save a temporary variable
+            and use it in read()
+        */
+        if (ssl_sock_temp_char != -1) {
+            return 1;
+        }
+        count = ::recv(*sock_fd, &ssl_sock_temp_char, 1, MSG_DONTWAIT);
+    } else {
+        zsock_ioctl(*sock_fd, ZFD_IOCTL_FIONREAD, &count);
+    }
+    if (count <= 0) {
+        delay(1);
+        count = 0;
+    }
+    return count;
+}
+
+int ZephyrSocketWrapper::recv(uint8_t *buffer, size_t size, int flags) {
+    if (sock_fd == nullptr || *sock_fd == -1) {
+        return -1;
+    }
+
+    // TODO: see available()
+    if (ssl_sock_temp_char != -1) {
+        int ret = ::recv(*sock_fd, &buffer[1], size - 1, flags);
+        buffer[0] = ssl_sock_temp_char;
+        ssl_sock_temp_char = -1;
+        return ret + 1;
+    }
+    return ::recv(*sock_fd, buffer, size, flags);
+}
+
+int ZephyrSocketWrapper::send(const uint8_t *buffer, size_t size) {
+    if (sock_fd == nullptr || *sock_fd == -1) {
+        return -1;
+    }
+
+    return ::send(*sock_fd, buffer, size, 0);
+}
+
+void ZephyrSocketWrapper::close() {
+    if (sock_fd) {
+        sock_fd = nullptr;
+    }
+}
+
+bool ZephyrSocketWrapper::bind(uint16_t port) {
+    struct sockaddr_in addr;
+    int raw_sock_fd;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    raw_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sock_fd =
+        std::shared_ptr<int>(raw_sock_fd < 0 ? nullptr : new int(raw_sock_fd), socket_deleter);
+    if (!sock_fd) {
+        return false;
+    }
+
+    zsock_ioctl(*sock_fd, ZFD_IOCTL_FIONBIO);
+
+    if (::bind(*sock_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        sock_fd = nullptr;
+        return false;
+    }
+
+    return true;
+}
+
+bool ZephyrSocketWrapper::listen(int backlog) {
+    if (sock_fd == nullptr || *sock_fd == -1) {
+        return false;
+    }
+
+    if (::listen(*sock_fd, backlog) < 0) {
+        sock_fd = nullptr;
+        return false;
+    }
+
+    return true;
+}
+
+int ZephyrSocketWrapper::accept() {
+    if (sock_fd == nullptr || *sock_fd == -1) {
+        return -1;
+    }
+
+    return ::accept(*sock_fd, nullptr, nullptr);
+}
+
+String ZephyrSocketWrapper::remoteIP() {
+    if (sock_fd == nullptr || *sock_fd == -1) {
+        return {};
+    }
+
+    struct sockaddr_storage addr;
+    socklen_t addr_len = sizeof(addr);
+    char ip_str[INET6_ADDRSTRLEN] = {0};
+
+    if (::getpeername(*sock_fd, (struct sockaddr *)&addr, &addr_len) == 0) {
+        if (addr.ss_family == AF_INET) {
+            struct sockaddr_in *s = (struct sockaddr_in *)&addr;
+            ::inet_ntop(AF_INET, &s->sin_addr, ip_str, sizeof(ip_str));
+        } else if (addr.ss_family == AF_INET6) {
+            struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)&addr;
+            ::inet_ntop(AF_INET6, &s6->sin6_addr, ip_str, sizeof(ip_str));
+        }
+        return String(ip_str);
+    }
+
+    return {};
+}
