f415feeae1cb5d226d954f428891ea43d9fb70a4
Andrea Gilardoni <a.gilardoni@arduino.cc>
2026-09-09 17:41:21 +0200

    fixup! loader: add ota support

--- cores/arduino/ota_api.h
+++ cores/arduino/ota_api.h
@@ -1,1 +1,11 @@
-loader/ota/ota_api.h
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef CONFIG_OTA
+
+#include "../../loader/ota/ota_api.h"
+
+#endif // CONFIG_OTA
